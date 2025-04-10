afc41cb0d794c5685b1002dc893f13ff3a0d7240
marcelohas19 <marcelo.hsousa19@gmail.com>
2025-04-10 08:31:15 -0300

    .

--- LanHouse/LanHouse/Arquivo.java
+++ LanHouse/LanHouse/Arquivo.java
@@ -7,10 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Arquivo {
-    public void arquivo(){
+    public void lerArquivo(){
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("Clientes.txt"))) {
             for(Cliente cl : clientes){
-            bw.write(cl.getNome() + " " + cl.getCpf() + " " + cl.getTelefone() + " " + cl.getPlano() + " " + cl.getMinutos() + "\n");
+                bw.write(cl.getNome() + " " + cl.getCpf() + " " + cl.getTelefone() + " " + cl.getPlano() + " " + cl.getMinutos() + "\n");
             }
         } catch (IOException e) {
             System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
